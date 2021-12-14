fe48564c2bfbcb5c3f4a85a38c768f78a6857923
Alessio Mancini <a.mancini@goreply.io>
2021-12-14 16:40:11 +0100

    primo commit per uplodare i file su azure repos

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "test_rg" {
+    name = "testalex-rg"
+    location = "${var.locations.location2}"
+}
+
+data "azurerm_resource_group" "resource_group" {
+  name = var.resource_group
+}
+
+output "location_RG" {
+  value = data.azurerm_resource_group.resource_group.location
+}
+output "ID_RG" {
+  value = data.azurerm_resource_group.resource_group.id
+}
+
---
+++ variables.tf
@@ -0,0 +1,42 @@
+#variable "subscription_id" {
+#    type = string
+#}
+#
+#variable "client_id" {
+#    type = string
+#}
+#
+#variable "client_secret" {
+#    type = string
+#}
+#
+#variable "tenant_id" {
+#    type = string
+#}
+
+variable "resource_group" {
+    type = string
+}
+
+
+variable "server_name" {
+    default = "web-server"     
+}
+
+variable "locations" {
+    type = map
+    default = {
+        location1 = "westus2"
+        location2 = "East US"
+    }
+}
+
+variable "subnets" {
+    type = list
+    default = ["10.0.1.10","10.0.1.11"] 
+}
+
+variable "live" {
+  type = string
+  default = false
+}
